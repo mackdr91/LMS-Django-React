32d59e20cb0bf10c000ceb51066b1eee2f9fef95
mackdr91 <domonick.r.mack+github@gmail.com>
2024-11-12 20:11:06 -0500

    added models to api app; add moviepy to project

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,11 @@
+from datetime import timedelta
 from django.db import models
 from userauth.models import CustomUser, Profile
 from django.utils.text import slugify
 from shortuuid.django_fields import ShortUUIDField
 from django.utils import timezone
+from moviepy.editor import VideoFileClip
+import math
 
 LANGUAGE_CHOICES = (
         ('en', 'English'),
@@ -145,3 +148,168 @@ class Course(models.Model):
             self.slug = slugify(self.title)
         super(Course, self).save(*args, **kwargs)
 
+    def students(self):
+        """
+        Return a queryset of enrolled students in the course.
+
+        This method filters and retrieves all enrolled courses associated
+        with the course instance, providing access to all students that are
+        enrolled in the course.
+
+        :return: A queryset of EnrolledCourse objects
+        :rtype: QuerySet
+        """
+        return EnrolledCourse.objects.filter(course=self)
+
+    def curriculum(self):
+        """
+        Return a queryset of VariantItem objects related to the course.
+
+        This method filters and retrieves all variant items associated
+        with the course instance, providing access to all curriculum items
+        that are part of the course.
+
+        :return: Queryset of VariantItem objects for this course
+        :rtype: QuerySet
+        """
+        return VariantItem.objects.filter(varriant__course=self)
+
+    def lectures(self):
+        """
+        Return a queryset of VariantItem objects related to the course.
+
+        This method filters and retrieves all variant items associated
+        with the course instance, providing access to all lectures
+        that are part of the course.
+
+        :return: Queryset of VariantItem objects for this course
+        :rtype: QuerySet
+        """
+        return VariantItem.objects.filter(varriant__course=self)
+
+    def average_rating(self):
+        """
+        Return the average rating for the course.
+
+        This method aggregates the average of all CourseReview objects
+        associated with the current course instance, providing an overview
+        of the course's overall rating.
+
+        :return: Average rating for this course
+        :rtype: float
+        """
+        average_rating = Review.objects.filter(course=self, active=True).aggregate(avg_rating=models.Avg('rating'))
+        return average_rating['avg_rating']
+
+    def rating_count(self):
+        """
+        Return the count of CourseReview objects associated with this course.
+
+        This method filters and retrieves all reviews where the course
+        is the current instance, allowing access to all reviews for
+        the course.
+
+        :return: Count of CourseReview objects for this course
+        :rtype: int
+        """
+        return Review.objects.filter(course=self, active=True).count()
+
+
+    def reviews(self):
+        """
+        Return a queryset of CourseReview objects associated with this course.
+
+        This method filters and retrieves all reviews where the course
+        is the current instance, allowing access to all reviews for
+        the course.
+
+        :return: Queryset of CourseReview objects for this course
+        :rtype: QuerySet
+        """
+        return Review.objects.filter(course=self, active=True)
+
+
+class Variant(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    variant_id = ShortUUIDField(unique=True, max_length=20, alphabet="1234567890", length=6)
+    slug = models.SlugField(unique=True,null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = 'Variants'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+    def variant_items(self):
+        """
+        Return a queryset of VariantItem objects associated with this variant.
+
+        This method filters and retrieves all variant items where the variant
+        is the current instance, allowing access to all variant items for
+        the variant.
+
+        :return: Queryset of VariantItem objects for this variant
+        :rtype: QuerySet
+        """
+        return VariantItem.objects.filter(variant=self)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Variant, self).save(*args, **kwargs)
+
+
+class VariantItem(models.Model):
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, related_name='variant_items')
+    title = models.CharField(max_length=500)
+    description = models.TextField(null=True, blank=True)
+    file = models.FileField(upload_to='course-file')
+    duration = models.DurationField(null=True, blank=True)
+    content_duration = models.CharField(max_length=1000, null=True, blank=True)
+    preview = models.BooleanField(default=False)
+    variant_item_id = ShortUUIDField(unique=True, max_length=20, alphabet="1234567890", length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = 'Variant Items'
+        ordering = ('title',)
+
+    def __str__(self):
+        return f"{self.variant.title} - {self.title}"
+
+
+    def save(self, *args, **kwargs):
+        """
+        Save the VariantItem and calculate the duration of the file.
+
+        This method overrides the default save method to calculate the duration
+        of the uploaded file and save it in the content_duration field.
+
+        :param \*args: Additional positional arguments to be passed to the parent class's
+            ``save()`` method.
+        :param \*\*kwargs: Additional keyword arguments to be passed to the parent class's
+            ``save()`` method.
+        :return: None
+        :rtype: NoneType
+        """
+        super().save(*args, **kwargs)
+
+        if self.file:
+            clip = VideoFileClip(self.file.path) # Replace with the path to your video file
+            duration_seconds = clip.duration # Get the duration in seconds
+
+            minutes, remainder = divmod(duration_seconds, 60) # 60 seconds in a minute example: 100 seconds = 1 minute and 40 seconds
+            minutes = math.floor(minutes) # Convert the minutes to an integer
+            seconds = math.floor(remainder) # Convert the seconds to an integer
+
+            duration = f"{minutes:02d}m:{seconds:02d}s" # Format the duration as "mm:ss" example: 1m:40s
+            self.content_duration = duration # Save the duration in the content_duration field
+
+            super().save(update_fields=['content_duration']) # Update only the content_duration field
